6e9d65e2fd8d9020626b092044b1743b691498ac
Jose Negrin <soyjosenegrin@gmail.com>
2022-04-26 20:46:46 +0200

    added columns with price change at 7d, 14d and 30d

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     const getCoins = async () => {
       if (data.length === 0) {
-        let URL: string = `${import.meta.env.VITE_API_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`! as string
+        let URL: string = `${import.meta.env.VITE_API_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C14d%2C30d`! as string
         const request = await fetch(URL)
         if (request.ok) {
           const response = await request.json()
@@ -36,11 +36,14 @@ function App() {
           <th>Nombre</th>
           <th>Precio</th>
           <th>24h</th>
+          <th>7d</th>
+          <th>14d</th>
+          <th>30d</th>
           <th>Capitalización</th>
         </tr>
         {data?.length > 0 && data.map((coins) => {
 
-          const { id, symbol, name, image, market_cap, current_price, market_cap_rank, price_change_percentage_24h } = coins
+          const { id, symbol, name, image, market_cap, current_price, market_cap_rank, price_change_percentage_24h, price_change_percentage_7d_in_currency, price_change_percentage_14d_in_currency, price_change_percentage_30d_in_currency } = coins
 
           return (
             <tr className='coin' key={id}>
@@ -49,7 +52,9 @@ function App() {
                 <span><img src={image} alt={id} width='25px' height='25px' /></span>{name} <span className='coin_symbol'>{symbol}</span></td>
               <td>{formatter.format(current_price)}</td>
               <td>{formatterPercent(price_change_percentage_24h)}</td>
-              {/* <td>{price_change_percentage_24h}</td> */}
+              <td>{formatterPercent(price_change_percentage_7d_in_currency)}</td>
+              <td>{formatterPercent(price_change_percentage_14d_in_currency)}</td>
+              <td>{formatterPercent(price_change_percentage_30d_in_currency)}</td>
               <td>{formatter.format(market_cap)}</td>
             </tr>
           )
